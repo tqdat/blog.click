9c223394b60ca0a37d08045e5fb8891779f6eec3
tqdat <quocdat23894@gmail.com>
2018-01-11 09:23:01 +0700

    config phần mở rộng 3

--- admin/modules/khachhang/views/add.php
+++ admin/modules/khachhang/views/add.php
@@ -1,119 +1,90 @@
 <link type="text/css" rel="stylesheet" href="<?php echo base_url().'templates/css/uploadify.css'; ?>" media="screen" />
 <script type="text/javascript" src="<?php echo base_url().'templates/js/core/jquery.uploadify.js'; ?>"></script>
 <?php echo form_open(uri_string(), array('id'=>'admindata'));?>
-<table class="form">
-    <tr>
-        <td class="label" style="width: 150px;">Tiêu đề</td>
-        <td><input type="text" name="vdata[title]" value="<?php echo set_value('vdata[title]')?>" class="w500"></td>
-    </tr>
-    <tr>
-        <td class="label">Giới thiệu</td>
-        <td><textarea style="width: 500px;" rows="4" name="vdata[des]"></textarea></td>
-    </tr>
-</table>
-<table style="width: 100%; margin-top: 20px;">
-    <tr>
-        <td style="width: 200px;">
-            <div class="imgmain" style="border: 1px solid #CCC; background: #FFF;width: 150px;height: 100px;margin-bottom: 10px;">
-                
+<div class="row">
+    <div class="col-md-12">
+        <div class="box box-primary">
+            <form role="form">
+                <div class="box-body">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Tiêu đề</label>
+                        <input type="text" name="vdata[title]" value="<?php echo set_value('vdata[title]')?>" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Giới thiệu</label>
+                        <textarea class="form-control" rows="4" name="vdata[des]"></textarea>
+                    </div>
+                    <div>
+                        <input type="hidden" name="vdata[images]" id="images">
+                        <input id="file_upload" name="file_upload" type="file" />
+                    </div>
+                    <div id="file_uploadQueue" class="uploadifyQueue"></div>
+                    <div class="form-group">
+                        <ul class="listimg">
+                            <?foreach($img as $val):?>
+                                <li>
+                                    <div class="img" img_src="<?=$val->path?>"><img src="<?=base_url_site()?>data/tam/<?=$val->path?>"></div>
+                                    <div class="l_del"><a href="javascript:;" img_id="<?=$val->id?>" img_src="<?=$val->path?>" class="del_img">Xóa</a></div>
+                                    <input type="hidden" name="ar_img[]" value="<?=$val->path?>">
+                                </li>
+                                <?endforeach;?>
+                            </ul> 
+                        </div>
+                    </div><!-- /.box-body -->
+                </form>
             </div>
-            <input type="hidden" name="vdata[images]" id="images">
-            <div id="block_upload"><input id="file_upload" name="file_upload" type="file" /></div>
-            <div id="file_uploadQueue" class="uploadifyQueue" style="width: 200px;"></div>
-        </td>
-        <td>
-            <ul class="listimg">
-            <?foreach($img as $val):?>
-            <li>
-                  <div class="img" img_src="<?=$val->path?>"><img src="<?=base_url_site()?>data/tam/<?=$val->path?>"></div>
-                  <div class="l_del"><a href="javascript:;" img_id="<?=$val->id?>" img_src="<?=$val->path?>" class="del_img">Xóa</a></div>
-                  <input type="hidden" name="ar_img[]" value="<?=$val->path?>">
-            </li>
-            <?endforeach;?>
-            </ul>  
-        </td>
-    </tr>
-</table>
-<?php echo form_close();?>
-<script type="text/javascript">
-$(document).ready(function() {
-    
-    $('#file_upload').uploadify({
-        'swf'  : base_url+'templates/swf/uploadify.swf',
-        'uploader'    : base_url+'khachhang/uploader/<?=$this->session->sessionid()?>',    
-        'width': 130,
-        'height'    : 30,
-        'queueID'        : 'file_uploadQueue',
-        'scriptAccess'  : 'always',
-        'fileTypeDesc'  : '*.jpg;*.png;*.gif',
-        'fileTypeExts'  : '*.jpg;*.png;*.gif',
-        'fileSizeLimit'  : (204800 * 1024),
-        'onUploadSuccess' : function(file, data, response) {
-            obj = $.parseJSON(data);
-            if(obj.error == 0){
-                var html ='<li>'+
-                          '<div class="img" img_src="'+obj.filename+'"><img src="'+base_url_site+'data/tam/'+obj.filename+'"></div>'+
-                          '<div class="l_del"><a href="javascript:;" img_id="'+obj.id+'" img_src="'+obj.filename+'" class="del_img">Xóa</a></div>'+
-                          '<input type="hidden" name="ar_img[]" value="'+obj.filename+'">'+
-                          '</li>';
-                $(".listimg").append(html);
-            }else{
-                alert(obj.msg);
-            }
-        }     
-    });
-    
-    $(".del_img").live('click',function(){
-        var img_src = $(this).attr('img_src');
-        var img_old = $("#images").val();
-        var id = $(this).attr('img_id');
-        
-        $.post(base_url+"khachhang/del_img_tam",{'id':id},function(data){
-            if(img_src == img_old){
-                $("#images").val('');
-                $(".imgmain").html('');
-            }
-        });
-            
-        
-        $(this).parent().parent().remove();
-    })
-    
-    $(".img").live('click',function(){
-        img_src = $(this).attr('img_src');
-        $(".imgmain").html('<img src="'+base_url_site+'data/tam/'+img_src+'">');
-        $("#images").val(img_src);
-    })
-})
-</script>
-<style type="text/css">
-.imgmain img{
-    width: 150px;
-    height: 100px;
-}
-ul.listimg li{
-    float: left;
-    width: 90px;
-    height: 80px;
-    margin-right: 10px;
-    margin-bottom: 10px;
-}
-ul.listimg li .img{
-    width: 90px;
-    height: 60px;
-    border: 1px solid #CCC;
-    background: #FFF;
-    display: block;
-    overflow: hidden;
-    cursor: pointer;
-}
-ul.listimg li .img img{
-    width: 90px;
-    min-height : 60px;
-}
-ul.listimg .l_del{
-    margin-top: 5px;
-    font-weight: bold;
-    text-align: center;
-}
-</style>
+        </div>
+    </div>
+
+    <?php echo form_close();?>
+    <script type="text/javascript">
+        $(document).ready(function() {
+
+            $('#file_upload').uploadify({
+                'swf'  : base_url+'templates/swf/uploadify.swf',
+                'uploader'    : base_url+'khachhang/uploader/<?=$this->session->sessionid()?>',    
+                'width': 130,
+                'height'    : 30,
+                'queueID'        : 'file_uploadQueue',
+                'scriptAccess'  : 'always',
+                'fileTypeDesc'  : '*.jpg;*.png;*.gif',
+                'fileTypeExts'  : '*.jpg;*.png;*.gif',
+                'fileSizeLimit'  : (204800 * 1024),
+                'onUploadSuccess' : function(file, data, response) {
+                    obj = $.parseJSON(data);
+                    if(obj.error == 0){
+                        var html ='<li>'+
+                        '<div class="img" img_src="'+obj.filename+'"><img src="'+base_url_site+'data/tam/'+obj.filename+'"></div>'+
+                        '<div class="l_del"><a href="javascript:;" img_id="'+obj.id+'" img_src="'+obj.filename+'" class="del_img">Xóa</a></div>'+
+                        '<input type="hidden" name="ar_img[]" value="'+obj.filename+'">'+
+                        '</li>';
+                        $(".listimg").append(html);
+                    }else{
+                        alert(obj.msg);
+                    }
+                }     
+            });
+
+            $(".del_img").live('click',function(){
+                var img_src = $(this).attr('img_src');
+                var img_old = $("#images").val();
+                var id = $(this).attr('img_id');
+
+                $.post(base_url+"khachhang/del_img_tam",{'id':id},function(data){
+                    if(img_src == img_old){
+                        $("#images").val('');
+                        $(".imgmain").html('');
+                    }
+                });
+
+
+                $(this).parent().parent().remove();
+            })
+
+            $(".img").live('click',function(){
+                img_src = $(this).attr('img_src');
+                $(".imgmain").html('<img src="'+base_url_site+'data/tam/'+img_src+'">');
+                $("#images").val(img_src);
+            })
+        })
+    </script>
--- admin/modules/slideshow/views/ds.php
+++ admin/modules/slideshow/views/ds.php
@@ -1,58 +1,69 @@
 <?php echo form_open('slideshow/dels',  array('id' => 'admindata'));?> 
 <input type="hidden" name="page" value="<?php echo set_post_page()?>">
-<table class="admindata">
-    <thead>
-        <tr class="pagination">
-            <th colspan="8">
-                Hiện có <?php echo $num?> ảnh slide <span class="pages"><?php echo $pagination?></span>
-            </th>
-        </tr>
-        <tr>
-            <th class="checked"><input type="checkbox" name="sa" id="sa" onclick="check_chose('sa', 'ar_id[]', 'admindata')"></th>
-            <th class="id">ID</th>
-            <th>Tên ảnh</th>
-            <th>Links</th>
-            <th>Ảnh</th>
-            <th>Sắp xếp <?php echo action_order()?></th>
-            <th class="fc">Chức năng</th>
-        </tr>        
-    </thead>
-    <?
-    $k=1;
-    foreach($list as $rs):
-    ?>
-    <tr class="row<?=$k?>">
-        <td align="center"><input  type="checkbox" name="ar_id[]" value="<?php echo $rs->id_slide?>"></td>
-        <td><?=$rs->id_slide?></td>
-        <td><a href="<?=site_url('slideshow/edit/'.$rs->id_slide)?>"><?=$rs->ten?></a></td>
-        <td><?=$rs->links?></td>
-        <td align="center"><img width="100" src="<?=base_url_site().'data/sl/'.$rs->images?>" alt=""></td>
-        <td align="center">
-            <input type="text" class="order" name="order_<?=$rs->id_slide?>" value="<?=$rs->ordering?>">
-            <input type="hidden" name="id[]" value="<?=$rs->id_slide?>">
-        </td>
-        <td align="center">
-            <span id="publish<?php echo $rs->id_slide?>"><?php echo icon_active("'slideshow'","'id_slide'",$rs->id_slide,$rs->published)?></span>
-            <?php echo icon_edit('slideshow/edit/'.$rs->id_slide)?>
-            <?php 
-                echo icon_del('slideshow/del/'.$rs->id_slide);
-            ?> 
-        </td>
-    </tr>       
-    <?php
-    $k=1-$k;
-    endforeach;
-    ?>
-    <tfoot>
-        <td colspan="8">
-            Hiện có <?php echo $num?> ảnh slide <span class="pages"><?php echo $pagination?></span>
-        </td>
-    </tfoot>    
-</table>
+<div class="row">
+    <div class="col-xs-12">
+        <div class="box">
+            <!-- /.box-header -->
+            <div class="box-body">
+                <table id="example2" class="table table-bordered table-hover">
+                    <thead>
+                        <tr class="pagination"> 
+                            <th>
+                                Hiện có <?php echo $num?> ảnh slide <span class="pages"><?php echo $pagination?></span>
+                            </th>
+                        </tr>
+                        <tr>
+                            <th class="checked" style="text-align: center;"><input type="checkbox" name="sa" id="sa" onclick="check_chose('sa', 'ar_id[]', 'admindata')"></th>
+                            <th class="id">ID</th>
+                            <th>Tên ảnh</th>
+                            <th>Links</th>
+                            <th>Ảnh</th>
+                            <th>Sắp xếp <?php echo action_order()?></th>
+                            <th>Chức năng</th>
+                        </tr>
+                    </thead>
+                    <?
+                    $k=1;
+                    foreach($list as $rs):
+                        ?>
+                        <tr class="row<?=$k?>">
+                            <td><input  type="checkbox" name="ar_id[]" value="<?php echo $rs->id_slide?>"></td>
+                            <td><?=$rs->id_slide?></td>
+                            <td><a href="<?=site_url('slideshow/edit/'.$rs->id_slide)?>"><?=$rs->ten?></a></td>
+                            <td><?=$rs->links?></td>
+                            <td><img width="100" src="<?=base_url_site().'data/sl/'.$rs->images?>" alt=""></td>
+                            <td>
+                                <input type="text" class="order" name="order_<?=$rs->id_slide?>" value="<?=$rs->ordering?>">
+                                <input type="hidden" name="id[]" value="<?=$rs->id_slide?>">
+                            </td>
+                            <td>
+                                <span id="publish<?php echo $rs->id_slide?>"><?php echo icon_active("'slideshow'","'id_slide'",$rs->id_slide,$rs->published)?></span>
+                                <?php echo icon_edit('slideshow/edit/'.$rs->id_slide)?>
+                                <?php 
+                                echo icon_del('slideshow/del/'.$rs->id_slide);
+                                ?> 
+                            </td>
+                        </tr>       
+                        <?php
+                        $k=1-$k;
+                    endforeach;
+                    ?>
+                    <tfoot>
+                        <td>
+                            Hiện có <?php echo $num?> ảnh slide <span class="pages"><?php echo $pagination?></span>
+                        </td>
+                    </tfoot> 
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 <?php echo form_close()?>
 
 <script type="text/javascript">
-function save_order(){
+    function save_order(){
     //load_show();
     var fields = $("#admindata :input").serializeArray();
     $.post(base_url+"slideshow/save_order",fields, function(data) {
